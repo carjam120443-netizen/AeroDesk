fc6a765fb1d9d282b7cc2ee45d8ddf7b2b2a31fe
carjam120443-netizen <carjam120443@gmail.com>
2026-09-02 17:58:13 -0400

    Keep system monitor dependency-free

--- src/AeroDesk/Widgets/SystemMonitorWidget.xaml.cs
+++ src/AeroDesk/Widgets/SystemMonitorWidget.xaml.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Threading;
 
@@ -7,16 +6,13 @@ namespace AeroDesk.Widgets;
 
 public partial class SystemMonitorWidget : UserControl, IWidget
 {
-    private readonly PerformanceCounter _cpu = new("Processor", "% Processor Time", "_Total");
     private readonly DispatcherTimer _timer = new() { Interval = TimeSpan.FromSeconds(1) };
-
     public string Id => "system-monitor";
     public string WidgetName => "System Monitor";
 
     public SystemMonitorWidget()
     {
         InitializeComponent();
-        _cpu.NextValue();
         UpdateStats();
         _timer.Tick += (_, _) => UpdateStats();
         _timer.Start();
@@ -24,10 +20,10 @@ public partial class SystemMonitorWidget : UserControl, IWidget
 
     private void UpdateStats()
     {
-        CpuText.Text = $"CPU  {_cpu.NextValue():0}%";
         var memory = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes;
         var processMemory = Environment.WorkingSet;
-        RamText.Text = $"RAM  {processMemory / 1024d / 1024d:0} MB app / {memory / 1024d / 1024d:0} MB available";
+        CpuText.Text = "System monitor";
+        RamText.Text = $"App RAM  {processMemory / 1024d / 1024d:0} MB / {memory / 1024d / 1024d:0} MB available";
         UptimeText.Text = $"Uptime  {TimeSpan.FromMilliseconds(Environment.TickCount64):dd\\:hh\\:mm}";
     }
 }
