6230ae66033e20d547a302cca9f5080cc0b146c9
carjam120443-netizen <carjam120443@gmail.com>
2026-09-02 17:36:54 -0400

    Add clock widget behavior

---
+++ src/AeroDesk/Widgets/ClockWidget.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace AeroDesk.Widgets;
+
+public partial class ClockWidget : UserControl, IWidget
+{
+    private readonly DispatcherTimer _timer = new() { Interval = TimeSpan.FromSeconds(1) };
+
+    public string Id => "clock";
+    public string Name => "Clock";
+
+    public ClockWidget()
+    {
+        InitializeComponent();
+        UpdateClock();
+        _timer.Tick += (_, _) => UpdateClock();
+        _timer.Start();
+    }
+
+    private void UpdateClock()
+    {
+        var now = DateTime.Now;
+        ClockText.Text = now.ToString("h:mm:ss tt");
+        DateText.Text = now.ToString("dddd, MMMM d, yyyy");
+    }
+}
