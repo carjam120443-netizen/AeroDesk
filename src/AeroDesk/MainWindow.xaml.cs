13490c1efa42553ff33bc166c28ece5843c5f4d9
carjam120443-netizen <carjam120443@gmail.com>
2026-09-02 17:37:19 -0400

    Load clock through widget host

--- src/AeroDesk/MainWindow.xaml.cs
+++ src/AeroDesk/MainWindow.xaml.cs
@@ -1,37 +1,20 @@
-using System;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
+using AeroDesk.Widgets;
 
 namespace AeroDesk;
 
 public partial class MainWindow : Window
 {
-    private readonly DispatcherTimer _clockTimer = new() { Interval = TimeSpan.FromSeconds(1) };
-
     public MainWindow()
     {
         InitializeComponent();
-        UpdateClock();
-        _clockTimer.Tick += (_, _) => UpdateClock();
-        _clockTimer.Start();
-    }
-
-    private void UpdateClock()
-    {
-        ClockText.Text = DateTime.Now.ToString("h:mm:ss tt");
-        DateText.Text = DateTime.Now.ToString("dddd, MMMM d, yyyy");
+        WidgetHost.AddWidget(new ClockWidget(), 0, 0);
     }
 
     private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
         if (e.LeftButton == MouseButtonState.Pressed)
             DragMove();
-    }
-
-    protected override void OnClosed(EventArgs e)
-    {
-        _clockTimer.Stop();
-        base.OnClosed(e);
     }
 }
